3cc7f7dc0cfac7baefa5dbc9aa222473096c2534
shrall <shrallvierdo@gmail.com>
2020-04-25 16:24:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/werewolf/mainmenu.java
@@ -0,0 +1,14 @@
+package com.example.werewolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+    }
+}
