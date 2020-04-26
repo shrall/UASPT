a9c7c16f58969541b1fccbd6da14f469f195f358
shrall <shrallvierdo@gmail.com>
2020-04-27 00:46:58 +0700

    Main Menu Progress
    Kurang :
    - Efek shadow di title
    - BGM

--- app/src/main/java/com/example/werewolf/mainmenu.java
+++ app/src/main/java/com/example/werewolf/mainmenu.java
@@ -2,13 +2,60 @@ package com.example.werewolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class mainmenu extends AppCompatActivity {
+
+    TextView textView;
+    Button startBtn, rolesBtn, settingsBtn, exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+        textView = (TextView) findViewById(R.id.title);
+        textView.setShadowLayer(1, 1, 2, Color.rgb(134, 133,239));
+
+        startBtn = findViewById(R.id.startbtn);
+        rolesBtn = findViewById(R.id.rolesbtn);
+        settingsBtn = findViewById(R.id.settingbtn);
+        exitBtn = findViewById(R.id.exitbtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainmenu.this, playersPage.class);
+                startActivity(intent);
+            }
+        });
+        rolesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainmenu.this, rolesPage.class);
+                startActivity(intent);
+            }
+        });
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainmenu.this, settingsPage.class);
+                startActivity(intent);
+            }
+        });
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Intent.ACTION_MAIN);
+                a.addCategory(Intent.CATEGORY_HOME);
+                a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(a);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/werewolf/playersPage.java
@@ -0,0 +1,14 @@
+package com.example.werewolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playersPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_page);
+    }
+}
