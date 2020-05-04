dd9f7021bd223498b6034c31d9bd4ec7a603a5de
shrall <shrallvierdo@gmail.com>
2020-05-04 18:26:15 +0700

    seer

---
+++ app/src/main/java/com/example/werewolf/activities/seerAction.java
@@ -0,0 +1,16 @@
+package com.example.werewolf.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.werewolf.R;
+
+public class seerAction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seer_action);
+    }
+}
