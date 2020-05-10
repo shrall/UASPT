be245f581d5d16d611b55e480bfd16d7815051f8
shrall <shrallvierdo@gmail.com>
2020-05-11 03:20:20 +0700

    Roles Page Progress
    done.

--- app/src/main/java/com/example/werewolf/activities/rolesPage.java
+++ app/src/main/java/com/example/werewolf/activities/rolesPage.java
@@ -15,7 +15,7 @@ import com.example.werewolf.R;
 
 public class rolesPage extends AppCompatActivity {
 
-    Button ww;
+    Button ww, vg, ht, kn, sr;
     Toolbar toolbar;
 
     @Override
@@ -34,20 +34,80 @@ public class rolesPage extends AppCompatActivity {
         });
 
 
-//        ww = findViewById(R.id.btn_ww);
-//        ww.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                wwDialog();
-//            }
-//        });
+        ww = findViewById(R.id.btn_ww);
+        ww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wwDialog();
+            }
+        });
+        vg = findViewById(R.id.btn_vilager);
+        vg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vgDialog();
+            }
+        });
+        ht = findViewById(R.id.btn_hunter);
+        ht.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                htDialog();
+            }
+        });
+        kn = findViewById(R.id.btn_guard);
+        kn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                knDialog();
+            }
+        });
+        sr = findViewById(R.id.btn_seer);
+        sr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                srDialog();
+            }
+        });
     }
 
-//    void wwDialog(){
-//        LayoutInflater inflater = LayoutInflater.from(this);
-//        View view = inflater.inflate(R.layout.ww_dialog, null);
-//        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
-//        alertDialog.show();
-//
-//    }
+    void wwDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.ww_dialog, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+    }
+
+    void vgDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.vg_dialog, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+    }
+
+    void srDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.sr_dialog, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+    }
+
+    void knDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.kn_dialog, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+    }
+
+    void htDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.ht_dialog, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+    }
 }
